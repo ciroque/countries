f5efb76d3d392dded3df59d5be81846737c670a7
Steve <ciroque@live.com>
2015-06-06 10:07:49 -0700

    Adding missing file.

--- src/test/scala/org/ciroque/countries/MockCountryDataLoader.scala
+++ src/test/scala/org/ciroque/countries/MockCountryDataLoader.scala
@@ -4,12 +4,13 @@ import org.ciroque.countries.model.Country
 
 object MockCountryDataLoader extends CountryDataLoader {
   override def load(): Option[List[Country]] = {
-    Some(
-      List(
-        Country("US", 0.0, 0.0, "USA", "United States of America"),
-        Country("MX", 0.0, 0.0, "Mexico", "Mexico"),
-        Country("CA", 0.0, 0.0, "Canada", "People's Republic of Canadia")
-      )
-    )
+    Some(countryList)
   }
+
+  val countryList = List(
+    Country("US", 0.0, 0.0, "USA", "United States of America"),
+    Country("MX", 0.0, 0.0, "Mexico", "Mexico"),
+    Country("CA", 0.0, 0.0, "Canada", "People's Republic of Canadia"),
+    Country("LU", 0.0, 0.0, "Luxembourg", "Grand Duchy of Luxembourg")
+  )
 }

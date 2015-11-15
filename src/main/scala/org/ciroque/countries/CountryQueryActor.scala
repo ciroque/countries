2ab012fc2a5bad3bfa0e9e02e3595d8f08519562
Steve <ciroque@live.com>
2015-11-14 19:35:39 -0800

    Minor refactor

--- src/main/scala/org/ciroque/countries/CountryQueryActor.scala
+++ src/main/scala/org/ciroque/countries/CountryQueryActor.scala
@@ -6,19 +6,28 @@ import org.ciroque.countries.queries.{CountryCodesQuery, CountryCodeQuery, Count
 
 class CountryQueryActor(countries: List[Country]) extends Actor {
   override def receive: Receive = {
-    case None => sender ! None
-    case empty: EmptyQuery => sender ! Some(countries)
-    case CountryCodeQuery(countryCode) =>  sender ! filterCountries(c => c.code.toLowerCase.equals(countryCode.toLowerCase))
+    case None =>
+      sender ! None
+
+    case empty: EmptyQuery =>
+      sender ! Some(countries)
+
+    case CountryCodeQuery(countryCode) =>
+      sender ! filterCountries(c => c.code.toLowerCase.equals(countryCode.toLowerCase))
+
     case CountryCodesQuery(countryCodes) =>
-      val lowerCased = countryCodes.map(c => c.toLowerCase)
-      sender ! filterCountries(c => lowerCased.contains(c.code.toLowerCase))
-    case CountryNameQuery(name) => sender ! filterCountries(c => c.name.toLowerCase.contains(name.toLowerCase))
-    case _ => println(s"!!!! CountryQueryActor:: Something else"); throw new IllegalArgumentException("Not sure what happened...")
+      sender ! filterCountries(c => countryCodes.map(c => c.toLowerCase).contains(c.code.toLowerCase))
+
+    case CountryNameQuery(name) =>
+      sender ! filterCountries(c => c.name.toLowerCase.contains(name.toLowerCase))
+
+    case _ =>
+      println(s"!!!! CountryQueryActor:: Something else"); throw new IllegalArgumentException("Not sure what happened...")
   }
 
   private def filterCountries(countryFilter: Country => Boolean) = {
     countries.filter(countryFilter) match {
-      case found if found.length == 0 => None
+      case found if found.isEmpty => None
       case found => Some(found)
     }
   }
